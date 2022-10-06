9546e5ab8e52786fc9ef0ee64f8ab45ba2d81849
Mattia <corradini.mattia05@gmail.com>
2022-10-06 21:34:04 +0200

    Aggiunge esercizio 7

--- esercizi_ricorsione/esercizi_compiti_23/es_7/src/Main.java
+++ esercizi_ricorsione/esercizi_compiti_23/es_7/src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
-
-    public
+    public static int sommaDispari(int[] v, int n){
+        if (n < 1)
+            return 0;
+        return sommaDispari(v, n - 2) + v[v.length - n];
+    }
 
     public static void main(String[] args) {
-
+        int DIM = 10;
+        int[] v = new int[DIM];
+        for (int i = 0; i < DIM; i++) {
+            v[i] = (int)(Math.random() * 10);
+        }
+        for (int i = 0; i < DIM; i++) {
+            System.out.print(v[i] + " ");
+        }
+        System.out.println(); System.out.println();
+        System.out.println("Somma dei posti dispari del vettore: " + sommaDispari(v, DIM));
     }
 }
