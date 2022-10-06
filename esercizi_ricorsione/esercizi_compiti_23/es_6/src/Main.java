2744d7ffeb76d4a4349c775cf661b0c7e9a778b8
Mattia <corradini.mattia05@gmail.com>
2022-10-06 21:27:00 +0200

    Aggiunge esercizio 6

--- esercizi_ricorsione/esercizi_compiti_23/es_6/src/Main.java
+++ esercizi_ricorsione/esercizi_compiti_23/es_6/src/Main.java
@@ -1,31 +1,31 @@
 public class Main {
-
-    public static boolean ricerca_bin(int[] v, int t, int i, int f){
-        if (i>=f)
+    public static boolean ricerca_bin(int[] v, int valore, int i, int f){
+        if (i > f)
             return false;
-        int centro=(i+f)/2;
-        if (v[centro]==t)
+        int pos = (i + f) / 2;
+        if (v[pos] == valore)
             return true;
-        else if (v[centro]<t)
-            return ricerca_bin(v, t, i, centro-1);
+        else if (v[pos] > valore)
+            return ricerca_bin(v, valore, i, pos - 1);
         else
-            return ricerca_bin(v, t, centro+1, f);
+            return ricerca_bin(v, valore, pos + 1, f);
     }
+
 
     public static void main(String[] args) {
         final int DIM=20;
-        int t=4, j=0;
+        int t=4;
         int[] v = new int[DIM];
         for (int i = 0; i < DIM; i++) {
-            v[i] = (int)(Math.random() * 10);
+            v[i] = i*10;
         }
         for (int i = 0; i < DIM; i++) {
             System.out.print(v[i] + " ");
         }
         System.out.println(); System.out.println();
-        if (!ricerca_bin(v, t, j, DIM))
+        if (!ricerca_bin(v, t, 0, DIM-1))
             System.out.println("il numero " + t + " non è presente nel vettore.");
-        if (ricerca_bin(v, t, j, DIM))
+        else
             System.out.println("il numero " + t + " è presente nel vettore.");
     }
 }
