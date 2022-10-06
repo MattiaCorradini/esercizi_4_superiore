068268e6afd136721328bb4a62191417e85fc2d7
Mattia <corradini.mattia05@gmail.com>
2022-10-06 20:36:16 +0200

    Aggiunge esercizio 3

---
+++ esercizi_ricorsione/esercizi_compiti_23/es_3/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static int massimo(int[] v, int i, int f){
+        if (i == f)
+            return v[i];
+        int o = massimo(v, i, (i + f) / 2);
+        int s = massimo(v, (i + f) / 2 + 1, f);
+        return Math.max(o, s);
+    }
+
+    public static void main(String[] args) {
+        int DIM = 15;
+        int[] v = new int[DIM];
+        for (int i = 0; i < DIM; i++) {
+            v[i] = (int)(Math.random() * 100);
+        }
+        for (int i = 0; i < DIM; i++) {
+            System.out.print(v[i] + " ");
+        }
+        System.out.println(); System.out.println();
+        System.out.println("il massimo degli elementi del vettore è: " + massimo(v, 0, DIM - 1));
+    }
+}
